f1d94c557bbfbdb87343d7889cfdd02a621142a8
Heather Gray <heathergray153@gmail.com>
2022-05-10 20:52:44 -0600

    WIP: test add functionality

--- test_termtodo.py
+++ test_termtodo.py
@@ -1,10 +1,20 @@
+import json
 import pytest
 from termtodo import add, delete, edit, lst
+from os.path import exists
+from os import remove
 
-def test_add(capsys):
+
+def test_setup():
+    if exists("./data/tasks.json"):
+        remove("./data/tasks.json")
+    assert not exists("./data/tasks.json")
+
+def test_add():
     add('test task', 'Work')
-    captured = capsys.readouterr()
-    assert captured.out == '"test task" added in Work\n'
+    with open("./data/tasks.json") as f:
+        test_task_list = json.load(f)
+    assert len(test_task_list) == 1
 
 def test_delete(capsys):
     delete(3)
