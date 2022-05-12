52eca64c027a5a045578286ace3b5ff170d6e4cf
Heather Gray <heathergray153@gmail.com>
2022-05-11 21:31:12 -0600

    add parens to call __str__ in untested edit method

--- task.py
+++ task.py
@@ -9,6 +9,7 @@ STATUS_PREFIXES = {
     "Cancelled": "[-]",
 }
 
+
 class Task:
     def __init__(self, id: int, task: str, tag: str, status: str):
 
@@ -18,28 +19,23 @@ class Task:
         self.status = status
 
     # UNTESTED
-    def edit(self, task: str = None, tag: str = None, 
-            status: str = None) -> str:
+    def edit(self, task: str = None, tag: str = None, status: str = None) -> str:
         if task is not None:
             self.task = task
         if tag is not None:
             self.tag = tag
         if status is not None:
             self.status = status
-        return self.__str__
+        return self.__str__()
 
     def encode(self):
         return self.__dict__
 
     @staticmethod
     def decode(json_dct):
-      return Task(json_dct['id'],
-                   json_dct['task'], 
-                   json_dct['tag'],
-                   json_dct['status'])
-
-    def __str__(self):
-        return (
-            f"{self.id}: {STATUS_PREFIXES[self.status]} {self.task} [{self.tag}]"
+        return Task(
+            json_dct["id"], json_dct["task"], json_dct["tag"], json_dct["status"]
         )
 
+    def __str__(self):
+        return f"{self.id}: {STATUS_PREFIXES[self.status]} {self.task} [{self.tag}]"
