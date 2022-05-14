7ab5ecda48d33240b49c68af9f6531a37be807f6
Heather Gray <heathergray153@gmail.com>
2022-05-13 20:43:11 -0600

    working edit test

--- test_termtodo.py
+++ test_termtodo.py
@@ -28,13 +28,18 @@ def test_add(task_list):
     add("test task", "Work")
     with open("./data/tasks.json") as f:
         test_task_list = json.load(f)
-    assert len(test_task_list) == 2
+    assert len(test_task_list) == 3
 
 
 def test_edit(task_list):
-    # TODO: Finish test
-    edit(1, text="test task edited")
-    edit(1, tag="test tag edit")
+    edit(1, text="test task edited", tag="test tag edit", status='In progress')
+    with open("./data/tasks.json") as f:
+        task_list = json.load(f, object_hook=Task.decode)
+    for task in task_list:
+        if task.id == 1:
+            assert task.text == 'test task edited'
+            assert task.tag == 'test tag edit'
+            assert task.status == 'In progress'
 
 
 def test_delete(task_list):
